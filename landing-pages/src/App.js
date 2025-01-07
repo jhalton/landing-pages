835457bd11cc1c70c0de4fb5a1df3057dddbbefc
Jeanette <haltonjeanette@gmail.com>
2025-01-06 16:59:23 -0800

    Change title, brain dump of how I want to approach these pages.

--- landing-pages/src/App.js
+++ landing-pages/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to The Landing page of Landing Pages</h1>
+      <p>
+        □ I think I would like to just have a grid of different landing pages.
+      </p>
+      <p>□ My initial goal is 8.</p>
+      <p>□ Each grid will have a title beneath.</p>
+      <p>
+        □ I am considering either showing a brief description when you hover
+        over the cell or...
+      </p>
+      <p>
+        □ When you get to the next landing page, a modal pops up with a brief
+        description and when you click it goes away.
+      </p>
+      <p>□ I think the first one feels less annoying as a user.</p>
     </div>
   );
 }
